40979cb16352dcaee15fe1691f35bcbae730c76d
Greg Bruce <GLBruce82@outlook.com>
2017-12-31 16:39:22 -0800

    Add assessment7 - Change the Name of a Playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,11 +5,24 @@ import './Playlist.css';
 
 //need to export this class?  Not in the instructions (yet?)
 export class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.props.onNameChange(e.target.value);
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue = {'New Playlist'}/>
-        <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval}/>
+        <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
+        <TrackList
+          tracks={this.props.playlistTracks}
+          onRemove={this.props.onRemove}
+          isRemoval={this.props.isRemoval}/>
         <a className="Playlist-save">SAVE TO SPOTIFY</a>
       </div>
     );
